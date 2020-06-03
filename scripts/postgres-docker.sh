a9c4651ae078980b5f9d115230c885f40b1b0224
bhargav <bhargavskumar51703@gmail.com>
2020-06-03 21:22:43 +0530

    added docker and sql scripts

--- scripts/postgres-docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --name my-postgres -v /home/samyak/pg_data:/var/lib/postgresql/data "$@" -e POSTGRES_PASSWORD=overdrive -d -p 5432:5432 postgres
--- scripts/setupDockerPostgres.sh
+++ scripts/setupDockerPostgres.sh
@@ -1,5 +1,6 @@
-
+#uncomment the below line while running for the first time
 #sudo docker pull postgres
-mkdir -p $HOME/mydocs/projects/v2/volumes/postgres
-sudo docker run  --name docker-postgres -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/mydocs/projects/v2/volumes/postgres:/var/lib/postgresql/data -v /home/ghost/mydocs/projects/v2/sql_scripts:/root/scripts/ postgres
+#in the below line provide path to where you want the postgres volume to be stored
+mkdir -p $HOME/path/to/volumes/postgres
+sudo docker run  --name docker-postgres -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/path/to/volumes/postgres:/var/lib/postgresql/data -v /home/path/to/sql_scripts:/root/scripts/ postgres
 echo "Container is running. To connect run the connect script"
